ce2d9c1cda13a4caa870b43e6b07d60393616d82
Alex Brasil <alexfbrazil123@gmail.com>
2021-02-21 02:22:30 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,17 +3,44 @@
 function generator1(){
     const links = [
         {
-          label: "Week1 notes",
+          label: "Week1",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week2",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week3",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week4",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week5",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week6",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week7",
+          url: "https://github.com/abrasil321/WWD330"
+        },
+        {
+          label: "Week8",
           url: "https://github.com/abrasil321/WWD330"
         }
       ];
-    
-    links.forEach(element => {
-        label = element.label;
-        url = element.url;
-
-        document.getElementById("links").innerHTML = "<li><a href='" + url + "'>" + label + "</a></li><br>";
-    });
+    let display = "";
+    for(let i = 0; i < links.length; i++){
+      display += "<li><a href='" + links[i].url + "'>" + links[i].label + "</a></li><br>";
+    }
+    document.getElementById("links").innerHTML = display;
 }
 
-//
+generator1();
+
